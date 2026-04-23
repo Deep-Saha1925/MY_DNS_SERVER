32297f5425563777e533451331a0c1081bd10534
Deep Saha <dip23447@gmail.com>
2026-04-23 22:53:56 +0530

    Created skleton for DNS server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Creating a UDP service
+const dgram = require('node:drgam');
+
+const server = dgram.createSocket('udp4')
+
+server.on('message', (msg, rinfo) => {
+    console.log('Incoming msg', msg.toString());
+})
+
+server.bind(53, () => {
+    console.log('DNS server running on port 53')
+})
