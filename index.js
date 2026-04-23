a896298cf87e3e12523037b05f292e14d2826e39
Deep Saha <dip23447@gmail.com>
2026-04-23 23:27:33 +0530

    Decoded incoming msg and formatted response

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 // Creating a UDP service
 const dgram = require('dgram');
+const dnsPacket = require('dns-packet');
 
-const server = dgram.createSocket('udp4')
+const server = dgram.createSocket('udp4');
+
+const db = {
+    'test.dev': '1.2.3.4',
+    'xyz.com': '4.5.6.7'
+}
 
 server.on('message', (msg, rinfo) => {
     //console.log('Incoming msg', msg.toString());
-    console.log({
-        msg: msg.toString(),
-        rinfo
-    });
+    const incomingReq = dnsPacket.decode(msg);
+    const ipFromDB = db[incomingReq.questions[0].name]
+
+    const ans = dnsPacket.encode({
+        type: 'response',
+        id: incomingReq.id,
+        flags: dnsPacket.AUTHORITATIVE_ANSWER,
+        questions: incomingReq.questions,
+        answers: [{
+            type: 'A',
+            class: 'IN',
+            name: incomingReq.questions[0].name,
+            data: ipFromDB
+        }]
+    })
+    server.send(ans, rinfo.port, rinfo.address)
 })
 
 server.bind(53, () => {
